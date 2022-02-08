6bb6d4a363772e830424f3e4d9705b6c016dc40b
bravi2 <bravi@inniveinc.com>
2022-02-08 11:57:11 +0530

    dropdown integrated

--- src/App.js
+++ src/App.js
@@ -11,10 +11,29 @@ class App extends React.Component {
             items: [],
             DataisLoaded: false
         };
+
+        this.handleChange = this.handleChange.bind(this)
+        this.doApiCall = this.doApiCall.bind(this)
     }
    
     // ComponentDidMount is used to
     // execute the code 
+
+doApiCall=(country)=>{
+  fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=ad8b358b24634005a0960f277c97c482`).then((res) => {
+              return res.json()})
+            .then((json) => {
+                this.setState({
+                    items: json.articles,
+                    DataisLoaded: true
+                });
+            })
+}
+
+    handleChange = (e)=>{
+console.log(e.target.value,'balaji');
+this.doApiCall(e.target.value);
+    }
     componentDidMount() {
         fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=ad8b358b24634005a0960f277c97c482").then((res) => {
               return res.json()})
@@ -33,10 +52,10 @@ class App extends React.Component {
         return (
           <div>
           <h1> Namshi Interview Task </h1> 
-          <select className="App-Dropdown">
-  <option value="uae">UAE</option>
+          <select className="App-Dropdown" onChange={this.handleChange}>
+  <option value="ae">UAE</option>
   <option value="in">India</option>
-  <option selected value="as">Australia</option>
+  <option selected value="au">Australia</option>
   <option value="us">America</option>
 </select>
         <div className = "tile" style={{display:"flex",flexWrap: "wrap"}}>
